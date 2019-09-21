44f9c5aa95d6c305e3d4f15311a1248b6c108086
Jan <37225011+jan-ITruns@users.noreply.github.com>
2019-09-21 10:02:58 +0200

    Bewegung
    Grundlegende Bewegung der Hauptfigur

---
+++ IronSky2/Assets/_Ws Jan/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player; 
+    public Vector3 offset;
+    
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.log(player.position);
+    
+        transform.position = player.position + offset;
+    }
+}
