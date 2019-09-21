f24caaac0b24568a7b71d8f3f1fa433f7aaa95d5
benediktpri <felix.2001@web.de>
2019-09-21 21:54:07 +0200

    Start Game button

---
+++ IronSky2/Assets/_Ws Bene/StartGame.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartTheGame()
+    {
+        Debug.Log("test");
+        //SceneManagement.LoadScene("Strecke_2");
+        //SceneManager.LoadScene("gameplay");
+                 Application.LoadLevel("Strecke_2");
+
+
+    }
+
+}
